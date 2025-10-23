be118d9149376b92a6419d144e190eed08155ca3
v087 <vpat0021@student.monash.edu>
2025-10-24 00:11:38 +1100

    Add files via upload

--- vis.js
+++ vis.js
@@ -4,7 +4,22 @@ vegaEmbed("#vis1", vg_1).then(function(result) {
 
 }).catch(console.error);
 
-var vg_2 =  "section3_visualisation.vg.json"
+
+var vg_2 =  "cdf.vg.json"
+
+vegaEmbed("#vis2", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_4 =  "heatmap.vg.json"
+
+vegaEmbed("#vis4", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_3 =  "section3_visualisation.vg.json"
 
 vegaEmbed("#vis3", vg_3).then(function(result) {
 
